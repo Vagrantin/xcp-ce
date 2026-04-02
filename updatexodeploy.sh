f11c27d2f88cb6d50c8ce83aa5744ae345bc1ca4
Matthieu Ducorps <sr.ht.qualify064@passmail.net>
2026-04-02 22:46:53 +0900

    c'est bizzar

--- updatexodeploy.sh
+++ updatexodeploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 grep -Irl 'xoa\.io' . --include='*.js' | head -n 1 | xargs -r sed -i 's|http://xoa.io/xva|https://xo-image.yawn.fi/downloads/image.xva.gz|g'
+grep -Irl 'lite\.xen-orchestra\.com' . | head -n 1 | xargs -r sed -i -e '/try {/,/\/\/ Fallback to local version of XO Lite/d' -e '/^        }$/d'
